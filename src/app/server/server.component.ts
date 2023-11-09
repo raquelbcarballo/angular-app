afbcbc87c3805bdeade325a05d927c79fb5aadbd
raquelbcarballo <raquelbc2996@gmail.com>
2023-11-09 19:35:10 +0100

    add comments in component selector types

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    // selector:'[app-server]'
+    // selector:'[app-server]' as atribute
+    // selector:'.app-server' as class
     templateUrl:'./server.component.html'
 })
 export class ServerComponent{
